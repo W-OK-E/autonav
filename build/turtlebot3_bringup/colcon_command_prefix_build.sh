d22999602d0fb1c0a2e662d855731070d3db61a3
W-OK-E <okhere21@gmail.com>
2025-03-31 22:11:00 +0530

    Removing build files

--- build/turtlebot3_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/w-ok-e/autonav/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/home/w-ok-e/autonav/install/turtlebot3_node/share/turtlebot3_node/package.sh"
--- build/turtlebot3_teleop/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/w-ok-e/autonav/install/turtlebot3_teleop'
