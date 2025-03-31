aa8e8a63a442a026601adfd7dfabf4167e25f78a
W-OK-E <okhere21@gmail.com>
2025-03-31 22:06:52 +0530

    Uploading Files

---
+++ build/turtlebot3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/w-ok-e/autonav/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/w-ok-e/autonav/install/turtlebot3_node/share/turtlebot3_node/package.sh"
